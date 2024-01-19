ee29d73826c4f37fe17d46271eb8ac46966a5ea6
Xiaoling Yi <143962462+xiaoling-yi@users.noreply.github.com>
2024-01-19 14:30:26 +0100

    Add PE src and test (#2)
    * add pe src and test
    
    * address comments
    
    * fix type
    
    * shift needs inside 0-63

---
+++ src/main/scala/simd/Parameter.scala
@@ -0,0 +1,15 @@
+package simd
+
+import chisel3._
+import chisel3.util._
+
+// parameters for post-processing SIMD accelerator
+object SIMDConstant {
+
+  // data width
+  def inputType = 32
+  def outputType = 8
+  def constantType = 8
+  def constantMulType = 32
+
+}
