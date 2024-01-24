50995ea0500bc181d9eae5117f963a2864f0f85f
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2024-01-24 13:53:14 +0100

    address commnets for 3+1 csr

--- src/main/scala/simd/Parameter.scala
+++ src/main/scala/simd/Parameter.scala
@@ -15,7 +15,7 @@ object SIMDConstant {
   // SIMD parallelism
   def laneLen = 64
 
-  // csrManager parameters, we use 3 csr for Post-processing SIMD
+  // csrManager parameters, we use 3 CSR for Post-processing SIMD configuration, + 1 for status CSR
   def csrNum: Int = 3 + 1
   def csrAddrWidth: Int = log2Up(csrNum)
 
