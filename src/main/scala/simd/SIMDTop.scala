a46b9cf175e4f48311a4af1c0501c0c848b00401
xiaoling-yi <xiaoling.yi@student.kuleuven.be>
2024-03-19 12:47:41 +0100

    modify module name

--- src/main/scala/simd/SIMDTop.scala
+++ src/main/scala/simd/SIMDTop.scala
@@ -13,44 +13,46 @@ class SIMDTop() extends Module with RequireAsyncReset {
 
   val io = IO(new SIMDTopIO())
 
-  val csrManager = Module(
+  val SIMDCsrManager = Module(
     new CsrManager(SIMDConstant.csrNum, SIMDConstant.csrAddrWidth)
   )
   val simd = Module(new SIMD())
 
-  // io.csr and csrManager input connection
-  csrManager.io.csr_config_in <> io.csr
+  // io.csr and SIMDCsrManager input connection
+  SIMDCsrManager.io.csr_config_in <> io.csr
 
-  // csrManager output and simd control port connection
+  // SIMDCsrManager output and simd control port connection
   // control signals
-  simd.io.ctrl.valid := csrManager.io.csr_config_out.valid
-  csrManager.io.csr_config_out.ready := simd.io.ctrl.ready
+  simd.io.ctrl.valid := SIMDCsrManager.io.csr_config_out.valid
+  SIMDCsrManager.io.csr_config_out.ready := simd.io.ctrl.ready
 
-  // splitting csrManager data ports to the simd configuration ports
+  // splitting SIMDCsrManager data ports to the simd configuration ports
   // the meanings of these ports can be found at PE.scala
   // the order is also the same as at PE.scala
   // as each control input port is 8 bits so 4 control input port shares 1 csr
-  simd.io.ctrl.bits.input_zp_i := csrManager.io.csr_config_out
+  simd.io.ctrl.bits.input_zp_i := SIMDCsrManager.io.csr_config_out
     .bits(0)(7, 0)
     .asSInt
-  simd.io.ctrl.bits.output_zp_i := csrManager.io.csr_config_out
+  simd.io.ctrl.bits.output_zp_i := SIMDCsrManager.io.csr_config_out
     .bits(0)(15, 8)
     .asSInt
 
   // this control input port is 32 bits, so it needs 1 csr
-  simd.io.ctrl.bits.multiplier_i := csrManager.io.csr_config_out.bits(2).asSInt
+  simd.io.ctrl.bits.multiplier_i := SIMDCsrManager.io.csr_config_out
+    .bits(2)
+    .asSInt
 
   simd.io.ctrl.bits.shift_i :=
-    csrManager.io.csr_config_out.bits(0)(23, 16).asSInt
+    SIMDCsrManager.io.csr_config_out.bits(0)(23, 16).asSInt
   simd.io.ctrl.bits.max_int_i :=
-    csrManager.io.csr_config_out.bits(0)(31, 24).asSInt
+    SIMDCsrManager.io.csr_config_out.bits(0)(31, 24).asSInt
 
   simd.io.ctrl.bits.min_int_i :=
-    csrManager.io.csr_config_out.bits(1)(7, 0).asSInt
+    SIMDCsrManager.io.csr_config_out.bits(1)(7, 0).asSInt
 
   // this control input port is only 1 bit
   simd.io.ctrl.bits.double_round_i :=
-    csrManager.io.csr_config_out.bits(1)(8).asBool
+    SIMDCsrManager.io.csr_config_out.bits(1)(8).asBool
 
   io.data <> simd.io.data
 
